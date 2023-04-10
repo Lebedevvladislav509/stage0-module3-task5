5c18087c768f54cbf92dc94a7398dbc5b5e439fc
LVLAD2009@yandex.ru <24233lebedko>
2023-04-10 15:34:25 +0300

    stage0-module3-task5

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        double seconds = minutes * 60d;
+        double seconds = minutes * 60.0;
         System.out.println(seconds);
     }
 }
